23fa7c2a42a3c312a09677b0c5c69cf5316b5f74
ajz34 <ajz34@outlook.com>
2021-05-23 13:34:14 +0800

    update example to check energy given by gradient is correct

--- examples/01-dh-grad.py
+++ examples/01-dh-grad.py
@@ -11,16 +11,25 @@ if __name__ == '__main__':
     #  [-0.16752  0.02649  0.02154]
     #  [ 0.02384 -0.03202  0.01631]
     #  [ 0.01762  0.01483  0.04038]]
-    print(RDFDH(mol, xc="XYG3").nuc_grad_method().run().de)
+    print(RDFDH(mol, xc="XYG3").run().e_tot)
+    mf = RDFDH(mol, xc="XYG3").nuc_grad_method().run()
+    print(mf.e_tot)
+    print(mf.de)
     # B2PLYP
     # [[ 0.13023 -0.00424 -0.07277]
     #  [-0.17088  0.02598  0.0211 ]
     #  [ 0.02338 -0.03631  0.01602]
     #  [ 0.01726  0.01457  0.03565]]
-    print(RDFDH(mol, xc="B2PLYP").nuc_grad_method().run().de)
+    print(RDFDH(mol, xc="B2PLYP").run().e_tot)
+    mf = RDFDH(mol, xc="B2PLYP").nuc_grad_method().run()
+    print(mf.e_tot)
+    print(mf.de)
     # MP2
     # [[ 0.1345  -0.00321 -0.07428]
     #  [-0.17439  0.02557  0.02063]
     #  [ 0.02301 -0.03636  0.01541]
     #  [ 0.01688  0.01401  0.03824]]
-    print(RDFDH(mol, xc="MP2").nuc_grad_method().run().de)
+    print(RDFDH(mol, xc="MP2").run().e_tot)
+    mf = RDFDH(mol, xc="MP2").nuc_grad_method().run()
+    print(mf.e_tot)
+    print(mf.de)
