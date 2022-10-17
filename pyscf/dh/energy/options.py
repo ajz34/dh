7f6a87097aa090333f2b0b0079965b7fbd516382
ajz34 <ajz34@outlook.com>
2022-10-17 13:26:40 +0800

    change options

--- pyscf/dh/energy/options.py
+++ pyscf/dh/energy/options.py
@@ -1,9 +1,5 @@
-from enum import Enum
-
-
 frozen_rule = None
-"""
-Rule for frozen orbital numbers.
+""" Rule for frozen orbital numbers.
 
 This option will be generate and be overrided by option ``frozen_list``.
 
@@ -15,24 +11,30 @@ TODO: Function to parse frozen rule.
 """
 
 frozen_list = None
-"""
-Index list of frozen orbitals.
+""" Index list of frozen orbitals.
 
 For example, if set to ``[0, 2, 3, 4]``, then those orbitals are not correlated in MP2 calculation.
 
 Default to None.
 """
 
+incore_t_ijab = "auto"
+""" Flag for tensor :math:`t_{ij}^{ab}` stored in memory or disk.
 
-class incore_t_ijab(Enum):
-    """
-    Flag for tensor :math:`t_{ij}^{ab}` stored in memory or disk.
-    """
-    true = True
-    """Store tensor in memory."""
-    false = False
-    """Store tensor in disk."""
-    auto = "auto"
-    """Leave program to judge whether tensor locates."""
-    default = auto
-    """Default to auto."""
+Parameters
+----------
+True
+    Store tensor in memory.
+False
+    Store tensor in disk.
+"auto"
+    Leave program to judge whether tensor locates.
+(int)
+    If tensor size exceeds this size (in MBytes), then store in disk. 
+"""
+
+coef_mp2_os = 1
+""" Coefficient of opposite-spin contribution to MP2 energy. """
+
+coef_mp2_ss = 1
+""" Coefficient of same-spin contribution to MP2 energy. """
