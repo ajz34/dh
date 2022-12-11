8090912f5833059eeeaf407bf4017a4ebc2f1e29
ajz34 <ajz34@outlook.com>
2022-12-11 19:44:25 +0800

    add ump2 frozen test

--- pyscf/test/energy/ump2.py
+++ pyscf/test/energy/ump2.py
@@ -11,3 +11,15 @@ def test_rmp2_conv():
         mf.driver_energy_mp2()
     print(mf.params.results)
     assert np.allclose(mf.params.results["eng_mp2"], -0.209174918573074)
+
+
+def test_rmp2_conv_fc():
+    mol = gto.Mole(atom="O; H 1 0.94; H 1 0.94 2 104.5", spin=1, charge=1, basis="cc-pVTZ").build()
+    mf_s = scf.UHF(mol).run()
+
+    mf = dh.energy.UDH(mf_s)
+    mf.params.flags["frozen_rule"] = "FreezeNobleGasCore"
+    with mf.params.temporary_flags({"integral_scheme": "conv"}):
+        mf.driver_energy_mp2()
+    print(mf.params.results)
+    assert np.allclose(mf.params.results["eng_mp2"], -0.195783018787701)
